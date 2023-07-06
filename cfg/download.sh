d8d01890138e1be6a1ab4beb3d96c5c2f1bd2c57
Riaan de Klerk ADM <RiaanDeKlerkADM@Qnetix.Cloud>
2023-07-06 14:44:49 +0100

    updated

--- cfg/download.sh
+++
@@ -1,3 +0,0 @@
-wget https://raw.githubusercontent.com/qnetix-io/qn-zprxy-base/main/cfg/init.sh
-wget https://raw.githubusercontent.com/qnetix-io/qn-zprxy-base/main/cfg/menu-appliance.sh
-wget https://raw.githubusercontent.com/qnetix-io/qn-zprxy-base/main/cfg/menu-size.sh
